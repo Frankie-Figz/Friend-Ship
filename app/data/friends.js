134c79dd935d21db0ecccfb7f712360aa6b2c39d
FrankieFigz <fjvannini@gmail.com>
2019-10-29 21:58:04 -0400

    Added the match friend algorithm and display friend to modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,33 +1,33 @@
 var friends = [
     {
-    name : "FJVR",
-    photo : "insert URL here",
-    scores : [1,2,3,4,5,1,2,3,4,5]
+        name : "FJVR",
+        photo : "insert URL here",
+        score : [1,2,3,4,5,1,2,3,4,5]
     },
     {
-        name : "A",
-        photo : "insert URL here",
-        scores : [1,1,1,1,1,1,1,1,1,1]
+        name : "Albert Einstein",
+        photo : "https://upload.wikimedia.org/wikipedia/commons/d/d3/Albert_Einstein_Head.jpg",
+        score : [1,1,1,1,1,1,1,1,1,1]
     },
     {
-        name : "B",
-        photo : "insert URL here",
-        scores : [2,2,2,2,2,2,2,2,2,2]
+        name : "Mahatma Gandhi",
+        photo : "https://www.publicdomainpictures.net/pictures/30000/velka/gandhi.jpg",
+        score : [2,2,2,2,2,2,2,2,2,2]
     },
     {
-        name : "C",
-        photo : "insert URL here",
-        scores : [3,3,3,3,3,3,3,3,3,3]
+        name : "Nelson Mandela",
+        photo : "https://dmn-dallas-news-prod.cdn.arcpublishing.com/resizer//Pv5xrxbnPLjG2D-_1_k1VeGHbsw=/1660x934/smart/filters:no_upscale()/arc-anglerfish-arc2-prod-dmn.s3.amazonaws.com/public/6OLRPCV7MAWMGGULWSNT4D7AII.jpg",
+        score : [3,3,3,3,3,3,3,3,3,3]
     },
     {
-        name : "D",
-        photo : "insert URL here",
-        scores : [4,4,4,4,4,4,4,4,4,4]
+        name : "Melinda Gates",
+        photo : "http://26t4l93f9dhe439yxm286lpv.wpengine.netdna-cdn.com/wp-content/uploads/2016/08/8620278610_a050f2ed88_o.jpg",
+        score : [4,4,4,4,4,4,4,4,4,4]
     },
     {
-        name : "E",
-        photo : "insert URL here",
-        scores : [5,5,5,5,5,5,5,5,5,5]
+        name : "Reese Witherspoon",
+        photo : "https://m.media-amazon.com/images/M/MV5BMTc3MDcxMDA5Ml5BMl5BanBnXkFtZTgwNDM1NTU5MDE@._V1_SY1000_CR0,0,684,1000_AL_.jpg",
+        score : [5,5,5,5,5,5,5,5,5,5]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,21 @@
 // Dependencies
 // =============================================================
+
 var express = require("express");
 var path = require("path");
 
 var friendsData = require("../data/friends");
 
+var Math = require("Mathjs");
+
 module.exports = function(app){
 
+  let localMinimum = {
+    name: "",
+    value: 100,
+    photo:""
+  };
+  
   // A GET route with the url `/api/friends`. 
   // This will be used to display a JSON of all possible friends.
   app.get("/api/friends", function(req, res) {
@@ -17,20 +26,39 @@ module.exports = function(app){
   // This will be used to handle incoming survey results. 
   // This route will also be used to handle the compatibility logic.
   app.post("/api/friends", function(req, res) {
+    
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newPerson = req.body;
-    console.log("I am here !");
+
     // Using a RegEx Pattern to remove spaces from newCharacter
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
     // newPerson.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
 
-    console.log(newPerson);
+    // res.json(friendsData);
 
-    friendsData.push(newPerson);
+    for(var i = 0; i < friendsData.length; i++){
+      var currentScore = 0;
 
-    res.json(friendsData);
+      for(var j in newPerson.score){
+        console.log(newPerson);
+        currentScore = currentScore + Math.abs(friendsData[i].score[j] - parseInt(newPerson.score[j]));
+        console.log(currentScore); 
+      }
+
+      if(currentScore < localMinimum.value){
+        localMinimum.value = currentScore;
+        localMinimum.name = friendsData[i].name;
+        localMinimum.photo = friendsData[i].photo;
+      }
+    }
+
+    console.log(localMinimum);
+
+    res.json(localMinimum);
+
+    // console.log(localMininum.name);
+    // console.log(localMininum.value);
+
   });
-
-
 }
